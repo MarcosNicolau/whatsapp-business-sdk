87590b92be73f212f7c448fcd2ed76f9f6f82d25
nicolau <marcosnicolau2003@gmail.com>
2022-05-20 21:54:43 -0300

    feat: project initialization!!
    started the whatsapp business api connector with typescript project!

---
+++ README.md
@@ -0,0 +1,9 @@
+# WhatsApp Business API Sdk
+
+Node.js connector for the WhatsApp APIs, with TypeScript support.
+
+This project offers a solution to easily interact with WhatsApp Business APIs. Built with Axios and no other extra dependency!
+
+# Documentation
+
+Most methods accept JS objects. These can be populated using parameters specified by WhatsApp's API documentation.
---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	setupFilesAfterEnv: ["./__tests__/setup.ts"],
+	roots: ["<rootDir>"],
+	modulePaths: [compilerOptions.baseUrl],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+	// Indicates whether the coverage information should be collected while executing the test
+	collectCoverage: true,
+	// The directory where Jest should output its coverage files
+	coverageDirectory: "coverage",
+	// Indicates which provider should be used to instrument code for coverage
+	coverageProvider: "v8",
+	testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest",
+	},
+};
