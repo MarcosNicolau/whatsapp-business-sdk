af3d4eff95821a5ec7db0a3659507ff3064d4e68
nicolau <marcosnicolau2003@gmail.com>
2022-05-23 22:00:42 -0300

    fix: 🐛 wABA error handler default message
    defaulted to API message if the error code is not present in our object

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -2,7 +2,7 @@ import { DefaultWABAErrorAPI } from "./../types/error";
 import { ERROR_CODES, WABAErrorAPI } from "types/error";
 
 export const WABAErrorHandler = (error: DefaultWABAErrorAPI) => {
-	const message = ERROR_CODES[error?.error?.code];
+	const message = ERROR_CODES[error?.error?.code] || error?.error?.message;
 	const err: WABAErrorAPI = { ...error?.error, message };
 	return Promise.reject(err);
 };
