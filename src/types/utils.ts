95e1b90b70dd3d7cd1f3cfc7110843636563172f
nicolau <marcosnicolau2003@gmail.com>
2022-05-23 21:59:43 -0300

    docs: 📚️ utils types

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,8 +1,43 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-export type LiteralUnion<T extends U, U = string> = T | (U & {});
-// eslint-disable-next-line @typescript-eslint/ban-types
-export type LiteralNumberUnion<T extends U, U = number> = T | (U & {});
+/**
+ * Adds string autocompletion as if it was a union, but allows other string values
+ *
+ * @example
+ * let foo = LiteralUnion<"foo" | "bar">;
+ * //I can compare it with another value and typescript does not complain!
+ * //And I still have autocompletion!
+ * if (foo === "other_value") return;
+ */
+export type LiteralUnion<T extends U, U = string> = T | (U & {}); // eslint-disable-line @typescript-eslint/ban-types
+/**
+ * Adds number autocompletion as if it was a union, but allows other number values
+ *
+ * @example
+ * let foo = LiteralUnion<0| 1>;
+ * //I can compare it with another value and typescript does not complain!
+ * //And I still have autocompletion!
+ * if (foo === 1) return;
+ */
+export type LiteralNumberUnion<T extends U, U = number> = T | (U & {}); // eslint-disable-line @typescript-eslint/ban-types
 
+/**
+ * Takes a type and maps all its values to never
+ * This is mostly used keeping autocompletion with objects union
+ *
+ * @example
+ * type Foo = {
+ * 		property: string;
+ * }
+ *
+ * type Bar = {
+ * 		property_2: string;
+ * }
+ *
+ * //I lose autocompletion because properties are not the same;
+ * type ObjectUnion = Foo | Bar;
+ *
+ * //The great autocompletion is back again!
+ * type ObjectUnionFixed = GeneratedMappedNever<Foo> | Bar;
+ */
 export type GenerateMappedNever<T> = {
 	[key in keyof T]: never;
 };
