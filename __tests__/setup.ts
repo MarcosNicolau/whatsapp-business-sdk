42d874b3b3a59857155757a8964df589597f43a9
nicolau <marcosnicolau2003@gmail.com>
2022-05-29 22:06:51 -0300

    test: 🚨 increased jest setTimeout
    increased jest setTimeout to 30 seconds to prevent errors because the API was slow

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,3 +1,7 @@
 import path from "path";
 import { config } from "dotenv";
 config({ path: path.resolve(__dirname, "..", ".env") });
+
+//Setting a large timeout because we are making real API calls
+//And some of them might take some time
+jest.setTimeout(30 * 10000);
