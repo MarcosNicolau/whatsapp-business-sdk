cb9a72adb6728e4017f6fc2f9f40ce63f2a562e7
lukas-becker0 <117725216+lukas-becker0@users.noreply.github.com>
2024-11-13 14:40:46 +0100

    chore(languageCodes): Fix formatting

--- src/types/languageCodes.ts
+++ src/types/languageCodes.ts
@@ -1,78 +1,78 @@
 // See the docs
 // here: https://developers.facebook.com/docs/whatsapp/business-management-api/message-templates/supported-languages/
 // and/or here: https://developers.facebook.com/docs/whatsapp/api/messages/message-templates/#supported-languages
-export declare const SUPPORTED_LANGUAGE_CODES: {
-	readonly Afrikaans: "af";
-	readonly Albanian: "sq";
-	readonly Arabic: "ar";
-	readonly Azerbaijani: "az";
-	readonly Bengali: "bn";
-	readonly Bulgarian: "bg";
-	readonly Catalan: "ca";
-	readonly Chinese_CHN: "zh_CN";
-	readonly Chinese_HKG: "zh_HK";
-	readonly Chinese_TAI: "zh_TW";
-	readonly Croatian: "hr";
-	readonly Czech: "cs";
-	readonly Danish: "da";
-	readonly Dutch: "nl";
-	readonly English: "en";
-	readonly English_UK: "en_GB";
-	readonly English_US: "en_US";
-	readonly Estonian: "et";
-	readonly Filipino: "fil";
-	readonly Finnish: "fi";
-	readonly French: "fr";
-	readonly Georgian: "ka";
-	readonly German: "de";
-	readonly Greek: "el";
-	readonly Gujarati: "gu";
-	readonly Hausa: "ha";
-	readonly Hebrew: "he";
-	readonly Hindi: "hi";
-	readonly Hungarian: "hu";
-	readonly Indonesian: "id";
-	readonly Irish: "ga";
-	readonly Italian: "it";
-	readonly Japanese: "ja";
-	readonly Kannada: "kn";
-	readonly Kazakh: "kk";
-	readonly Kinyarwanda: "rw_RW";
-	readonly Korean: "ko";
-	readonly Kyrgyzstan: "ky_KG";
-	readonly Lao: "lo";
-	readonly Latvian: "lv";
-	readonly Lithuanian: "lt";
-	readonly Macedonian: "mk";
-	readonly Malay: "ms";
-	readonly Malayalam: "ml";
-	readonly Marathi: "mr";
-	readonly Norwegian: "nb";
-	readonly Persian: "fa";
-	readonly Polish: "pl";
-	readonly Portuguese_BR: "pt_BR";
-	readonly Portuguese_PT: "pt_PT";
-	readonly Punjabi: "pa";
-	readonly Romanian: "ro";
-	readonly Russian: "ru";
-	readonly Serbian: "sr";
-	readonly Slovak: "sk";
-	readonly Slovenian: "sl";
-	readonly Spanish: "es";
-	readonly SpanishArgentina: "es_AR";
-	readonly SpanishSpain: "es_ES";
-	readonly SpanishMexico: "es_MX";
-	readonly Swahili: "sw";
-	readonly Swedish: "sv";
-	readonly Tamil: "ta";
-	readonly Telugu: "te";
-	readonly Thai: "th";
-	readonly Turkish: "tr";
-	readonly Ukrainian: "uk";
-	readonly Urdu: "ur";
-	readonly Uzbek: "uz";
-	readonly Vietnamese: "vi";
-	readonly Zulu: "zu";
-};
+export const SUPPORTED_LANGUAGES_CODES = {
+	Afrikaans: "af",
+	Albanian: "sq",
+	Arabic: "ar",
+	Azerbaijani: "az",
+	Bengali: "bn",
+	Bulgarian: "bg",
+	Catalan: "ca",
+	Chinese_CHN: "zh_CN",
+	Chinese_HKG: "zh_HK",
+	Chinese_TAI: "zh_TW",
+	Croatian: "hr",
+	Czech: "cs",
+	Danish: "da",
+	Dutch: "nl",
+	English: "en",
+	English_UK: "en_GB",
+	English_US: "en_US",
+	Estonian: "et",
+	Filipino: "fil",
+	Finnish: "fi",
+	French: "fr",
+	Georgian: "ka",
+	German: "de",
+	Greek: "el",
+	Gujarati: "gu",
+	Hausa: "ha",
+	Hebrew: "he",
+	Hindi: "hi",
+	Hungarian: "hu",
+	Indonesian: "id",
+	Irish: "ga",
+	Italian: "it",
+	Japanese: "ja",
+	Kannada: "kn",
+	Kazakh: "kk",
+	Kinyarwanda: "rw_RW",
+	Korean: "ko",
+	Kyrgyzstan: "ky_KG",
+	Lao: "lo",
+	Latvian: "lv",
+	Lithuanian: "lt",
+	Macedonian: "mk",
+	Malay: "ms",
+	Malayalam: "ml",
+	Marathi: "mr",
+	Norwegian: "nb",
+	Persian: "fa",
+	Polish: "pl",
+	Portuguese_BR: "pt_BR",
+	Portuguese_PT: "pt_PT",
+	Punjabi: "pa",
+	Romanian: "ro",
+	Russian: "ru",
+	Serbian: "sr",
+	Slovak: "sk",
+	Slovenian: "sl",
+	Spanish: "es",
+	SpanishArgentina: "es_AR",
+	SpanishSpain: "es_ES",
+	SpanishMexico: "es_MX",
+	Swahili: "sw",
+	Swedish: "sv",
+	Tamil: "ta",
+	Telugu: "te",
+	Thai: "th",
+	Turkish: "tr",
+	Ukrainian: "uk",
+	Urdu: "ur",
+	Uzbek: "uz",
+	Vietnamese: "vi",
+	Zulu: "zu",
+} as const;
 
-export type SupportedLanguagesCodeUnion = typeof SUPPORTED_LANGUAGE_CODES[keyof typeof SUPPORTED_LANGUAGE_CODES];
+export type SupportedLanguagesCodeUnion = typeof SUPPORTED_LANGUAGES_CODES[keyof typeof SUPPORTED_LANGUAGES_CODES];
